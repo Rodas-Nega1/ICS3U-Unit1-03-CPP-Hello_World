1a07b5db5f1547dfe8d46de86f64958124bf6706
Ubuntu <ubuntu@ip-172-31-74-112.ec2.internal>
2021-09-10 22:04:37 +0000

    hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Rodas Nega
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+}
